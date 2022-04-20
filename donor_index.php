c2b8d0ceb95171a3c1b296d4b187887ddc9e90c0
shuvo-99 <anontoshuvo99@gmail.com>
2022-04-21 03:30:29 +0600

    first commit

---
+++ donor_index.php
@@ -0,0 +1,111 @@
+
+<?php
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+	$user_data = check_login($con);
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+
+		$dtype = $_POST['dtype'];
+		$user_id =  $user_data['user_id'] ;
+
+			if ($dtype==="money"){
+				header("Location: money_donor.php");
+			}
+			if ($dtype==="product"){
+				header("Location: product_donor.php");
+			}
+			die;
+		}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Easy Charity </title>
+  </head>
+  <body>
+		<head>
+ <title>Welcome to Easy Charity!</title>
+ <style type="text/css">
+  h1 { text-align: center}
+ </style>
+<body>
+ <h1> <font size="+2"> Welcome to Easy Charity! </font></H1>
+</body>
+
+
+
+			<h1>  <font size="1">Hello <?php echo $user_data['user_name'] ?>, This is your profile. You are  our verified donor.</font>
+</h1>
+
+			 	<h1>  <font size="2"> Want to make donation? Find all places and people you can donate right here:
+
+			 </font>
+					<br>
+  <font size="-1">
+		<style type="text/css">
+	   h1 { text-align: center}
+	  </style>
+		<h1>
+
+
+
+						<table style="border:1px solid black;margin-left:auto;margin-right:auto;">
+							<tr>
+								<th>  <font size="2"> Receiver Name </font></th>
+								<th>  <font size="2"> Receiver ID </font></th>
+							</tr>
+							<?php
+
+								$con= mysqli_connect("localhost","root","", "charity");
+								$sql= 'SELECT * from users WHERE utype="receiver"';
+								$result= $con->query($sql);
+
+								if ($result->num_rows>0){
+									while($row= $result-> fetch_assoc()){
+									 echo	 "<tr><td>".$row['user_name'] . "</td><td>" .$row['user_id'] . "</td>" ;
+									}
+								}
+								else {
+									echo "No results";
+								}
+
+
+								$con->close();
+							 ?>
+						</table>
+						<form method="post">
+
+
+
+			        <label for="utype">Choose your donation type:</label>
+			        <select name="dtype" id="dtype">
+			            <option value="money">Money (cash)</option>
+			          <option value="product">Item/Product</option>
+
+			        </select>
+
+			        <input  id="button" type="submit"  value="Press to Proceed">
+
+							 <br>
+
+
+
+		         </select>
+
+
+						</font>
+						</h1>
+					<br><br>
+			     <br><br>
+					 <br><br>
+		<a href="logout.php">Logout</a>
+  </body>
+</html>
---
+++ product_donor.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+     <form action = "">
+       <div class = "container">
+       <h1> Donate Products </h1>
+       <p> Please fill in the boxes to complete donation </p>
+       <hr/>
+       
+       <label for = "type"><b> Product Type </b> </label>
+       <input type="text" placeholder = "Product Type" name = 'type' id = 'type' required/>
+       
+       <label for = "qnty"><b> Product Quantity </b> </label>
+       <input type="text" placeholder = "Product Quantity" name = 'qnty' id = 'qnty' required/>
+       
+       <label for = "address"><b> Address </b> </label>
+       <input type="text" placeholder = "Address" name = 'address' id = 'address' required/>
+       </hr>
+    
+       <button class = "subtmitbtn" type = "submit"> Submit </button>
+    </form>
+    
+  </div> 
+  </body>
+</html>
